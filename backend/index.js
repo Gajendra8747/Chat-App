ef0bbdd64803ee92c989cccdcf8f11cbdc91f1c9
Gajendra_Gour <145483617+Gajendra8747@users.noreply.github.com>
2025-02-26 10:19:14 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: "https://chat-app-frontend-five-kohl.vercel.app",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST"]
   }
 });
